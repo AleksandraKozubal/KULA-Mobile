bc07c588d1bddb5ce7ffef12a0a171eb4f9f7e2b
Lee0z <leeos10100@gmail.com>
2025-01-17 20:04:23 +0100

    Fix import path casing for FillingModel in tests

--- test/Data/filling_model_test.dart
+++ test/Data/filling_model_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kula_mobile/data/models/filling_model.dart';
+import 'package:kula_mobile/Data/Models/filling_model.dart';
 
 void main() {
   group('FillingModel', () {
