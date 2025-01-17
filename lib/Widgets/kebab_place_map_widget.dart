163b2974b9816e60631f73e54dd9aaae7f3c9c32
Lee0z <leeos10100@gmail.com>
2025-01-17 18:06:01 +0100

    Add kebab place fetching and display on map with details dialog

--- lib/Widgets/kebab_place_map_widget.dart
+++ lib/Widgets/kebab_place_map_widget.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'package:kula_mobile/Data/Data_sources/kebab_place_data_source.dart';
+import 'package:kula_mobile/Data/Repositories/kebab_place_repository_impl.dart';
+import 'package:kula_mobile/Data/Models/kebab_place_model.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'badge_widget.dart';
+import 'kebab_place_details_widget.dart';
+import 'package:kula_mobile/Data/Data_sources/filling_data_source.dart';
+import 'package:kula_mobile/Data/Data_sources/sauce_data_source.dart';
+import 'package:kula_mobile/Data/Repositories/filling_repository_impl.dart';
+import 'package:kula_mobile/Data/Repositories/sauce_repository_impl.dart';
 
 class KebabPlaceMapWidget extends StatefulWidget {
   const KebabPlaceMapWidget({super.key});
@@ -11,6 +22,36 @@ class KebabPlaceMapWidget extends StatefulWidget {
 
 class _KebabPlaceMapWidgetState extends State<KebabPlaceMapWidget> {
   final LatLng _initialPosition = const LatLng(51.2070, 16.1550);
+  List<KebabPlaceModel> _kebabPlaces = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchKebabPlaces();
+  }
+
+  Future<void> _fetchKebabPlaces({int page = 1}) async {
+    try {
+      final response = await KebabPlaceRepositoryImpl(
+        KebabPlaceDataSource(client: http.Client()),
+      ).getKebabPlaces(page: page);
+      setState(() {
+        _kebabPlaces = response['data'];
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Nie udało się załadować kebabów'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +65,151 @@ class _KebabPlaceMapWidgetState extends State<KebabPlaceMapWidget> {
           retinaMode: RetinaMode.isHighDensity(context),
           urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         ),
+        if (!_isLoading)
+          MarkerLayer(
+            markers: _kebabPlaces
+                .map(
+                  (kebabPlace) => Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: LatLng(
+                      double.parse(kebabPlace.latitude!),
+                      double.parse(kebabPlace.longitude!),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    kebabPlace.name,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.close),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            ),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Adres: ${kebabPlace.address}'),
+                                if (kebabPlace.googleMapsRating != null)
+                                  Row(
+                                    children: [
+                                      Text(
+                                          'Ocena: ${kebabPlace.googleMapsRating}'),
+                                      const SizedBox(width: 8.0),
+                                      RatingBarIndicator(
+                                        rating: double.parse(
+                                            kebabPlace.googleMapsRating!),
+                                        itemBuilder: (context, index) =>
+                                            const Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                        ),
+                                        itemCount: 5,
+                                        itemSize: 20.0,
+                                        direction: Axis.horizontal,
+                                      ),
+                                    ],
+                                  ),
+                                if (kebabPlace.phone != null)
+                                  Text('Telefon: ${kebabPlace.phone}'),
+                                if (kebabPlace.website != null)
+                                  Text(
+                                      'Strona internetowa: ${kebabPlace.website}'),
+                                const SizedBox(height: 4.0),
+                                Row(
+                                  children: [
+                                    if (kebabPlace.status == 'zamknięte')
+                                      const BadgeWidget(
+                                        text: 'Zamknięte',
+                                        color: Colors.red,
+                                        solid: true,
+                                      ),
+                                    if (kebabPlace.status == 'otwarte')
+                                      const BadgeWidget(
+                                        text: 'Otwarte',
+                                        color: Colors.green,
+                                        solid: true,
+                                      ),
+                                    if (kebabPlace.status == 'planowane')
+                                      const BadgeWidget(
+                                        text: 'Planowane',
+                                        color: Colors.orange,
+                                        solid: true,
+                                      ),
+                                    const SizedBox(width: 8.0),
+                                    if (kebabPlace.isCraft == true) ...[
+                                      const BadgeWidget(
+                                        text: 'Kraft',
+                                        color: Colors.purple,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                    ],
+                                    if (kebabPlace.openedAtYear != null) ...[
+                                      BadgeWidget(
+                                        text: 'Od ${kebabPlace.openedAtYear}',
+                                        color: Colors.deepOrangeAccent,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                    ],
+                                  ],
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            KebabPlaceDetailsWidget(
+                                          kebabPlace: kebabPlace,
+                                          fillingRepository:
+                                              FillingRepositoryImpl(
+                                            FillingDataSource(
+                                                client: http.Client()),
+                                          ),
+                                          sauceRepository: SauceRepositoryImpl(
+                                            SauceDataSource(
+                                                client: http.Client()),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Zobacz szczegóły'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: const Icon(
+                        Icons.fastfood,
+                        color: Colors.red,
+                        size: 25.0,
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
       ],
     );
   }
